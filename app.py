af795d4ce5d05d176ff11ea62a4dede3328cdcea
paige cruz <103955256+paigerduty@users.noreply.github.com>
2023-03-09 13:18:50 -0800

    fixes

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 from flask import Flask
-from redis import Redis
+import random
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
+
+hits = 0
 
 @app.route('/visit')
 def hello():
-    redis.incr('hits')
-    counter = str(redis.get('hits'),'utf-8')
-    return "This webpage has been viewed "+counter+" time(s)"
+    global hits
+    hits = hits + 1
+    return f'This webpage has been viewed {hits} times'
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route("/rolldice")
+def roll_dice():
+    return str(do_roll())
+
+def do_roll():
+    return random.randint(1, 6)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
